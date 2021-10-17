c653d7cf81f1911fd59b993f0bfb22f5565fc8d0
Winterbl00m <60327852+Winterbl00m@users.noreply.github.com>
2021-10-16 21:56:06 -0400

    Cleaned Hungre_games.py

--- code/hunger_games.py
+++ code/hunger_games.py
@@ -86,6 +86,23 @@ class HungerGames:
       self.G.nodes[node]["marksmanship"] = random.choice(list_of_marksmanships)
 
 
+  def random_duel(self, players):
+
+    if self.marksmanships[players[0]] == self.marksmanships[players[1]]:
+      return players
+      
+    self.duel_count += 1
+    while len(players) == 2 :
+      random.shuffle(players)
+      starting_player = players[0]
+      if flip(self.marksmanships[starting_player]):
+        other_player = players[1]
+        players.remove(other_player)
+        self.G.remove_node(other_player)
+
+    return players
+
+
   def sequential_duel(self, players, starting_player = None):
     players.sort(key=lambda player: self.marksmanships[player], reverse=False)
 
@@ -122,6 +139,7 @@ class HungerGames:
       #print("Tie!")
       return players
 
+    self.truel_count += 1
     #Formated x attack y (or z if x and y have same marksmanship)
     strongest_opponent_strategy = {c: [a,b], b: [a,c], a : [b, c]}
 
@@ -164,7 +182,8 @@ class HungerGames:
 
     elif len(self.G[players[0]]) == 1:
       players.extend(_random_subset(list(self.G[players[0]]), 1))
-      players = self.sequential_duel(players)
+      players = self.random_duel(players)
+      
     #Clear list after done
     players = []
 
@@ -174,18 +193,25 @@ class HungerGames:
     """
     G : graph
     """     
-    foo = list(self.G.degree())
-    nodes, degs = map(list,zip(*foo))
-    # nodes = [foo2[0] for foo2 in foo]
-    # degs = [foo2[1] for foo2 in foo]
+    degrees = list(self.G.degree())
+    nodes, degs = map(list,zip(*degrees))
 
-    for node in self.G.nodes:
-      if len(self.G[node]) == 0:
-        start_node = node
-        end_nodes = choices(nodes, degs, k = self.k)
-        edges = [(start_node, end_nodes[i]) for i in range(self.k)]
+    k = min([self.k, len(nodes) - 1])
 
-        self.G.add_edges_from(edges)
+    if k != 0:
+      for node in self.G.nodes:
+        if len(self.G[node]) == 0:
+          start_node = node
+          start_node_index = nodes.index(start_node) 
+          # print(start_node_index)
+
+          del nodes[start_node_index]
+          del degs[start_node_index]
+
+          end_nodes = choices(nodes, degs, k = k)
+          edges = [(start_node, end_nodes[i]) for i in range(k)]
+
+          self.G.add_edges_from(edges)
 
   """Now this next function defines the end case. The end-case holds to be true if, for every node in the graph:
   - The node has either no neighbors, or
@@ -204,7 +230,8 @@ class HungerGames:
     return True
 
   def loop(self):
-
+    self.truel_count =  0
+    self.duel_count = 0
     finished = False
     battle_count = 0
     # While there are less than 100,000 battles or the end case is not reached
@@ -231,43 +258,46 @@ class HungerGames:
 
 
 """Now, we can use Monte-Carlo experimentation to figure out the average number of red, blue, and black players, respectively, that will survive at the end of random conditions. We will loop through 10 random seeds, and for each starting condition, each one is run 10 times.
-
 (NOTE: Currently, this program runs through 10 million duels/truels and can take approximately a half-hour to run.)
 """
-
 def monte_carlo(n, k, seed, runs):
   """
   Runs the simulation k times for a random seed.
   """
   res = np.array([0.0,0.0,0.0])
   for run in range(runs):
-    print(run)
+    print("run:" +str(run))
     simulation = HungerGames(n, k, seed)
     winners = np.array(simulation.loop())
     res += winners
   return res/k
 
-#########
-res = np.array([0.0,0.0,0.0])
-for seed in range(10):
-  winners = monte_carlo(300, 10, seed, 10)
-  res += winners
-res = (res/10).tolist()
-#print(res)
+# #########
+# res = np.array([0.0,0.0,0.0])
+# for seed in range(10):
+#   winners = monte_carlo(300, 10, seed, 10)
+#   res += winners
+# res = (res/10).tolist()
+# #print(res)
 
-"""Create a bar graph of the results, by percentage."""
+# """Create a bar graph of the results, by percentage."""
 
-reds, blues, blacks = res
-sum = reds + blues + blacks
-percent_reds = reds/sum * 100
-percent_blues = blues/sum * 100
-percent_blacks = blacks/sum * 100
+# reds, blues, blacks = res
+# sum = reds + blues + blacks
+# percent_reds = reds/sum * 100
+# percent_blues = blues/sum * 100
+# percent_blacks = blacks/sum * 100
 
-x = ['reds', 'blues', 'blacks']
-y = [percent_reds, percent_blues, percent_blacks]
+# x = ['reds', 'blues', 'blacks']
+# y = [percent_reds, percent_blues, percent_blacks]
 
-plt.bar(x,y)
-plt.ylabel('Percentage of Winners (%)')
-plt.title('Average Percentages of Each Player Remaining')
-plt.show()
-"""The figure above shows that in truels where players are logical, red players, or the stronger players, are more likely to get eliminated, which lines up with the general conclusion of traditional game theory that stronger players will lose faster. On average, blue players and black players, with marksmanships `p = 0.8` and `p = 0.5`, respectively, make up approximately 84.4% of the players who survive the truels."""
+# plt.bar(x,y)
+# plt.ylabel('Percentage of Winners (%)')
+# plt.title('Average Percentages of Each Player Remaining')
+# plt.show()
+n, k = 300, 2
+seed = 3
+simulation = HungerGames(n, k, seed)
+winners = np.array(simulation.loop())
+print("truel_count:" + str(simulation.truel_count))
+print("duel_count:" + str(simulation.duel_count))
